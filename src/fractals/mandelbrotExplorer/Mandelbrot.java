15cc12afaad3787993854b37465be0a6ab681ac1
pxe778 <pxe778@student.bham.ac.uk>
2018-04-02 18:36:39 +0100

    better colouring system

--- src/fractals/mandelbrotExplorer/Mandelbrot.java
+++ src/fractals/mandelbrotExplorer/Mandelbrot.java
@@ -6,6 +6,8 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 import javax.swing.JFrame;
@@ -13,7 +15,7 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 
 public class Mandelbrot extends JPanel {
-	
+
 	/**
 	 * 
 	 */
@@ -21,51 +23,109 @@ public class Mandelbrot extends JPanel {
 	private int imagePixelsSquare;
 	private int sampleDepth;
 	private BufferedImage imageBuffer;
+	private int[] colourPalette;
+	private int[] histogram;
+	private int[] iterants;
+
+	private static final int BLACK = Color.BLACK.getRGB();
 
 	public Mandelbrot() {
+		// user input for certain attributes
 		Scanner input = new Scanner(System.in);
 		System.out.print("Canvas scale: ");
 		imagePixelsSquare = Integer.parseInt(input.next().trim());
 		System.out.print("Maximum depth pass limit: ");
 		sampleDepth = Integer.parseInt(input.next().trim());
-		setPreferredSize(new Dimension(imagePixelsSquare, imagePixelsSquare));
 		input.close();
+		setPreferredSize(new Dimension(imagePixelsSquare, imagePixelsSquare));
+		// sets up the colours
+		colourPalette = new int[100];
+		histogram = new int[sampleDepth + 1];
+		for (int i = 0; i < sampleDepth + 1; i++) {
+			histogram[i] = 0;
+		}
+		// inefficient but useful way to store values
+		iterants = new int[imagePixelsSquare * imagePixelsSquare];
+		preGenerateColours();
 		generateImage();
+		histogramColouring();
+	}
+
+	private void preGenerateColours() {
+		List<Double> xs = Arrays.asList(0.0, 16.0, 42.0, 64.25, 85.75, 100.0);
+		List<Double> ys = Arrays.asList(0.0, 0.125, 0.92578125, 1.0, 0.0, 0.0);
+		MonotoneCubicSplineInterpolator red = new MonotoneCubicSplineInterpolator(xs, ys);
+		ys = Arrays.asList(0.03125, 0.421875, 1.0, 0.6640625, 0.0078125, 0.03125);
+		MonotoneCubicSplineInterpolator green = new MonotoneCubicSplineInterpolator(xs, ys);
+		ys = Arrays.asList(0.390625, 0.7890625, 1.0, 0.0, 0.0, 0.390625);
+		MonotoneCubicSplineInterpolator blue = new MonotoneCubicSplineInterpolator(xs, ys);
+		for (int i = 0; i < colourPalette.length; i++) {
+			double thisRed = rangeCheck(red.interpolate(i * (100.0 / colourPalette.length)));
+			double thisGreen = rangeCheck(green.interpolate(i * (100.0 / colourPalette.length)));
+			double thisBlue = rangeCheck(blue.interpolate(i * (100.0 / colourPalette.length)));
+			colourPalette[i] = (new Color((float) thisRed, (float) thisGreen, (float) thisBlue)).getRGB();
+		}
 	}
 	
+	private double rangeCheck(double a) {
+		if (a < 0.0) {
+			return 0;
+		} else if (a > 1.0) {
+			return 1.0;
+		}
+		return a;
+	}
+
 	private void generateImage() {
 		imageBuffer = new BufferedImage(imagePixelsSquare, imagePixelsSquare, BufferedImage.TYPE_INT_RGB);
-		for(int pixelY = 0; pixelY < imagePixelsSquare; pixelY++) {
-			for(int pixelX = 0; pixelX < imagePixelsSquare; pixelX ++) {
+		for (int pixelY = 0; pixelY < imagePixelsSquare; pixelY++) {
+			for (int pixelX = 0; pixelX < imagePixelsSquare; pixelX++) {
 				Double x0 = (pixelX - imagePixelsSquare / 2.0) / imagePixelsSquare * 4.0;
 				Double y0 = (pixelY - imagePixelsSquare / 2.0) / imagePixelsSquare * 4.0;
 				Double x = 0.0;
 				Double y = 0.0;
 				int i;
-				for(i = 0; i < sampleDepth; i++) {
-					if(x*x + y*y >= 4) {
+				for (i = 0; i < sampleDepth; i++) {
+					if (x * x + y * y >= 4) {
 						break;
 					}
 					Double tempX = x * x - y * y + x0;
 					y = 2 * x * y + y0;
 					x = tempX;
 				}
-				if(i == sampleDepth) {
-					imageBuffer.setRGB(pixelX, pixelY, (Color.BLACK).getRGB());
-				}
-				else {
-					imageBuffer.setRGB(pixelX, pixelY, (new Color(Color.HSBtoRGB((float) (i/30.0), 1, 0.5f))).getRGB());
+				histogram[i]++;
+				iterants[pixelX + pixelY * imagePixelsSquare] = i;
+			}
+		}
+	}
+
+	private void histogramColouring() {
+		int total = 0;
+		for (int i = 0; i < histogram.length; i++) {
+			total += histogram[i];
+		}
+		for (int pixelY = 0; pixelY < imagePixelsSquare; pixelY++) {
+			for (int pixelX = 0; pixelX < imagePixelsSquare; pixelX++) {
+				if (iterants[pixelX + pixelY * imagePixelsSquare] == sampleDepth) {
+					imageBuffer.setRGB(pixelX, pixelY, BLACK);
+				} else {
+					float hue = 0f;
+					for (int i = 0; i < iterants[pixelX + pixelY * imagePixelsSquare]; i++) {
+						hue += histogram[i] / (float) (total);
+					}
+					imageBuffer.setRGB(pixelX, pixelY,
+							colourPalette[((int) (hue * colourPalette.length * 20 + colourPalette.length / 3))
+									% colourPalette.length]);
 				}
 			}
 		}
 	}
-	
+
 	@Override
 	public void paint(Graphics g) {
 		super.paint(g);
 		g.drawImage(imageBuffer, 0, 0, null);
 	}
-	
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
---
+++ src/fractals/mandelbrotExplorer/MonotoneCubicSplineInterpolator.java
@@ -0,0 +1,89 @@
+package fractals.mandelbrotExplorer;
+
+import java.util.List;
+
+public class MonotoneCubicSplineInterpolator {
+
+	private List<Double> xs;
+	private List<Double> ys;
+	private double[] ms;
+
+	public MonotoneCubicSplineInterpolator(List<Double> x, List<Double> y) {
+		this(x, y, new double[0]);
+	}
+
+	private MonotoneCubicSplineInterpolator(List<Double> _x, List<Double> _y, double[] _m) {
+		if (_x == null || _y == null || _x.size() != _y.size() || _x.size() < 2) {
+			throw new IllegalArgumentException("Both lists must be the same length and have length of at least 2.");
+		}
+		this.xs = _x;
+		this.ys = _y;
+		this.ms = _m;
+		processInterpolator();
+	}
+
+	private void processInterpolator() {
+		final int xSize = xs.size();
+		double[] midMidSemiMid = new double[xSize - 1];
+		ms = new double[xSize];
+		// compute slopes of secant lines between points
+		for (int i = 0; i < xSize - 1; i++) {
+			double h = xs.get(i + 1) - xs.get(i);
+			if (h <= 0f) {
+				throw new IllegalArgumentException("Values for the X list must be in ascending order.");
+			}
+			midMidSemiMid[i] = (ys.get(i + 1) - ys.get(i)) / h;
+		}
+		// initialise tangents as the average of secants
+		ms[0] = midMidSemiMid[0];
+		for (int i = 1; i < xSize - 1; i++) {
+			ms[i] = (midMidSemiMid[i - 1] + midMidSemiMid[i]) * 0.5f;
+		}
+		ms[xSize - 1] = midMidSemiMid[xSize - 2];
+		// update tangents to preserve monotonicity
+		for (int i = 0; i < xSize - 1; i++) {
+			if (midMidSemiMid[i] == 0f) { // successive y values are equal
+				ms[i] = 0f;
+				ms[i + 1] = 0f;
+			} else {
+				double a = ms[i] / midMidSemiMid[i];
+				double b = ms[i + 1] / midMidSemiMid[i];
+				double h = Math.hypot(a, b);
+				if (h > 9f) {
+					double t = 3f / h;
+					ms[i] = t * a * midMidSemiMid[i];
+					ms[i + 1] = t * b * midMidSemiMid[i];
+				}
+			}
+		}
+	}
+
+	public double interpolate(double x) {
+		// handle the boundary cases
+		final int xSize = xs.size();
+		if (Double.isNaN(x)) {
+			return x;
+		}
+		if (x <= xs.get(0)) {
+			return ys.get(0);
+		}
+		if (x >= xs.get(xSize - 1)) {
+			return ys.get(xSize - 1);
+		}
+		// find the index of the last point with smaller x
+		// we know this will be within the spline from the boundary tests
+		int i = 0;
+		while (x >= xs.get(i + 1)) {
+			i += 1;
+			if (x == xs.get(i)) {
+				return ys.get(i);
+			}
+		}
+		// perform cubic Hermite spline interpolation
+		double h = xs.get(i + 1) - xs.get(i);
+		double t = (x - xs.get(i)) / h;
+		return (ys.get(i) * (1 + 2 * t) + h * ms[i] * t) * (1 - t) * (1 - t)
+				+ (ys.get(i + 1) * (3 - 2 * t) + h * ms[i + 1] * (t - 1)) * t * t;
+	}
+
+}
